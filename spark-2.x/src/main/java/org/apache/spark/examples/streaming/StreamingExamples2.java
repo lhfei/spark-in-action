0724fe654ffaa1dfb899523c9785ad051b6e7987
Hefei Li <lhfeilaile@gmail.com>
2016-08-10 16:45:23 +0800

    add scala examples.

---
+++ spark-2.x/src/main/java/org/apache/spark/examples/streaming/StreamingExamples2.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2010-2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.spark.examples.streaming;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @version 1.0.0
+ *
+ * @author Hefei Li
+ *
+ * @since Jul 29, 2016
+ */
+public class StreamingExamples2 {
+
+	public static void setStreamingLogLevels() {
+		Logger root = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+	}
+}
